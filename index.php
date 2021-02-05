688096ec4454975883b392349f523c31039e9f97
zmh0007 <zmh0007@mix.wvu.edu>
2021-02-04 20:30:03 -0500

    Lab 2 - About Page

---
+++ index.php
@@ -0,0 +1,57 @@
+<main>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>Lightsabers - About Us</h1>
+        <p>qwertyuiopasdfghjklzxcvbnm</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/luke.jpeg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Luke Skywalker</p>
+                    <p>Email: LSkywalker@lightsabers.com</p>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/obiwan.jpeg">
+                <div class="container">
+                    <h2>CFO</h2>
+                    <p>Obi-Wan-Kenobi</p>
+                    <p>Email: helloThere@Lightsabers.com</p>
+                </div>
+
+            </div>
+        </div>
+    
+        <div class="column">
+            <div class="card">
+                <img src="images/Anakin.webp">
+                <div class="container">
+                    <h2>CFO</h2>
+                    <p>Anakin Skywalker</p>
+                    <p>Email: MasterSkywalker@Lightsabers.com</p>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/yoda.jpeg">
+                <div class="container">
+                    <h2>Founder</h2>
+                    <p>Yoda</p>
+                    <p>Email: Yoda@Lightsabers.com</p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</main>
