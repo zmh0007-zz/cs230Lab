d20bd2ae775277bb9133ddbf6392ecf73c598321
zmh0007 <zmh0007@mix.wvu.edu>
2021-02-04 22:37:55 -0500

    Lab 2 - About Page

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 <main>
     <link rel="stylesheet" href="css/about.css">
 
-    <div class="about">
-        <h1>Lightsabers - About Us</h1>
-        <p>qwertyuiopasdfghjklzxcvbnm</p>
+    <div class="about" >
+        <h1 style= "font-size: 48px" >Lightsabers - About Us</h1>
+        <p>Hello, Welcome to Lightsabers.com! This is our team! We are the official galactic producers and distributors 
+        of lightsaber blades and hilts. order yours today!</p>
+
     </div>
     <div class="row">
         <div class="column">
             <div class="card">
                 <img src="images/luke.jpeg">
                 <div class="container">
-                    <h2>CEO</h2>
+                    <h2 style= "text-decoration: underline 2px black">CEO</h2>
                     <p>Luke Skywalker</p>
-                    <p>Email: LSkywalker@lightsabers.com</p>
+                    <p style= "font-size: 12px">Email: LSkywalker@lightsabers.com</p>
                 </div>
 
             </div>
@@ -22,9 +24,9 @@
             <div class="card">
                 <img src="images/obiwan.jpeg">
                 <div class="container">
-                    <h2>CFO</h2>
+                    <h2 style= "text-decoration: 2px underline black" >CFO</h2>
                     <p>Obi-Wan-Kenobi</p>
-                    <p>Email: helloThere@Lightsabers.com</p>
+                    <p style= "font-size: 12px">Email: HelloThere@lightsabers.com</p>
                 </div>
 
             </div>
@@ -34,9 +36,9 @@
             <div class="card">
                 <img src="images/Anakin.webp">
                 <div class="container">
-                    <h2>CFO</h2>
+                    <h2 style= "text-decoration: underline 2px black">Website Manager</h2>
                     <p>Anakin Skywalker</p>
-                    <p>Email: MasterSkywalker@Lightsabers.com</p>
+                    <p style= "font-size: 12px">Email: MasterSkywalker@lightsabers.com</p>
                 </div>
 
             </div>
@@ -46,9 +48,9 @@
             <div class="card">
                 <img src="images/yoda.jpeg">
                 <div class="container">
-                    <h2>Founder</h2>
+                    <h2 style= "text-decoration: underline 2px black" >Founder</h2>
                     <p>Yoda</p>
-                    <p>Email: Yoda@Lightsabers.com</p>
+                    <p style= "font-size: 12px">Email: Yoda@lightsabers.com</p>
                 </div>
 
             </div>
