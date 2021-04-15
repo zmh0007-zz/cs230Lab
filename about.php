72fbf98ea04bb1dade0a047b79febf00cf8378bb
zmh0007 <zmh0007@mix.wvu.edu>
2021-04-15 14:36:04 -0400

    Testing

--- about.php
+++ about.php
@@ -7,12 +7,12 @@ require 'includes/header.php';
 <main>
 
     <link rel="stylesheet" href="css/about.css">
-   
 
-    <div class="about" >
-        <h1 style= "font-size: 48px" >Lightsabers - About Us</h1>
-        <p>Hello, Welcome to Lightsabers.com! This is our team! We are the official galactic producers and distributors 
-        of lightsaber blades and hilts. order yours today!</p>
+
+    <div class="about">
+        <h1 style="font-size: 48px">Lightsabers - About Us</h1>
+        <p>Hello, Welcome to Lightsabers.com! This site allows you to post your own lightsabers and review the
+            lightsabers of other users! May the force be with you!</p>
 
     </div>
 
@@ -21,9 +21,9 @@ require 'includes/header.php';
             <div class="card">
                 <img src="images/luke.jpeg">
                 <div class="container">
-                    <h2 style= "text-decoration: underline 2px black">CEO</h2>
+                    <h2 style="text-decoration: underline 2px black">CEO</h2>
                     <p>Luke Skywalker</p>
-                    <p style= "font-size: 12px">Email: LSkywalker@lightsabers.com</p>
+                    <p style="font-size: 12px">Email: LSkywalker@lightsabers.com</p>
                 </div>
 
             </div>
@@ -33,21 +33,21 @@ require 'includes/header.php';
             <div class="card">
                 <img src="images/obiwan.jpeg">
                 <div class="container">
-                    <h2 style= "text-decoration: 2px underline black" >CFO</h2>
+                    <h2 style="text-decoration: 2px underline black">CFO</h2>
                     <p>Obi-Wan-Kenobi</p>
-                    <p style= "font-size: 12px">Email: HelloThere@lightsabers.com</p>
+                    <p style="font-size: 12px">Email: HelloThere@lightsabers.com</p>
                 </div>
 
             </div>
         </div>
-    
+
         <div class="column">
             <div class="card">
                 <img src="images/Anakin.webp">
                 <div class="container">
-                    <h2 style= "text-decoration: underline 2px black">Website Manager, Not Jedi Master</h2>
+                    <h2 style="text-decoration: underline 2px black">Website Manager</h2>
                     <p>Anakin Skywalker</p>
-                    <p style= "font-size: 12px">Email: MasterSkywalker@lightsabers.com</p>
+                    <p style="font-size: 12px">Email: JediMasterSkywalker@lightsabers.com</p>
                 </div>
 
             </div>
@@ -57,20 +57,11 @@ require 'includes/header.php';
             <div class="card">
                 <img src="images/yoda.jpeg">
                 <div class="container">
-                    <h2 style= "text-decoration: underline 2px black" >Founder</h2>
+                    <h2 style="text-decoration: underline 2px black">Founder</h2>
                     <p>Yoda</p>
-                    <p style= "font-size: 12px">Email: Yoda@lightsabers.com</p>
+                    <p style="font-size: 12px">Email: Yoda@lightsabers.com</p>
                 </div>
-
             </div>
         </div>
-
-        <div class="footer">
-    <footer>Copyright Lightsabers.com</footer>
-
-    </div>
-
-
-
     </div>
 </main>
